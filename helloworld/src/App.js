4254cd42bf034518b0f400eaf236b1d16f39992c
Preetham Sridhar <preetham.prem2325@gmail.com>
2019-05-12 21:49:23 +0530

    css styling

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -12,13 +12,19 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import StyleSheets from './components/StyleSheets';
+import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
+import Form from './components/Form';
+
 
 class App extends Component {
 
     render() {
         return (
             <div className="App">
-                <NameList></NameList>
+                <Form></Form>
             </div>
         );
     }
